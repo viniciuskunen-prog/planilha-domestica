1fdde21a8360a197fe98f7acb2890d66c0f18c0f
viniciuskunen-prog <vinicius.kunen@gmail.com>
2026-05-20 11:56:58 -0300

    app: import month control styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './app/App.jsx';
 import './styles/globals.css';
+import './styles/month.css';
 
 createRoot(document.getElementById('root')).render(React.createElement(App));
