4102dfb324cb404163f661f150075215c2783db2
viniciuskunen-prog <vinicius.kunen@gmail.com>
2026-05-20 11:46:50 -0300

    app: add react entrypoint

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { App } from './app/App.jsx';
+import './styles/globals.css';
+
+createRoot(document.getElementById('root')).render(React.createElement(App));
